75235304123a2f7487065a778a54c8e5e454de2c
Théo VIDAL <theo@becauseofprog.fr>
2023-08-23 13:17:49 +0200

    chore: add meta, favicons and auto dark mode

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,6 +3,12 @@ import vuetify from 'vite-plugin-vuetify'
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
+  app: {
+    head: {
+      charset: 'utf-8',
+      viewport: 'width=device-width, initial-scale=1, viewport-fit=cover',
+    }
+  },
   css: ['vuetify/lib/styles/main.sass', '@mdi/font/css/materialdesignicons.min.css', 'assets/style.sass'],
   build: {
     transpile: ['vuetify'],
