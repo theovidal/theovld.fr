7c14b32bf68a1ec3c42e6a1f623e73f43ba0a8e8
Théo VIDAL <theo.vidal@ensta-paris.fr>
2024-09-26 19:49:52 +0200

    refactor: adjust banner style and add slots for bottom elements

--- components/banner.vue
+++ components/banner.vue
@@ -1,16 +1,53 @@
 <template>
-  <v-parallax :src="props.src">
+  <v-parallax
+    :src="props.src"
+    :class="{
+      'fullscreen': props.fullscreen,
+      'h-100': props.fill,
+      'shadow': props.shadow
+    }"
+    class="banner">
     <v-container
-      class="container d-flex justify-center"
+      class="container d-flex justify-center translucent"
       :class="{ 'align-center': center }">
-      <v-row>
+      <v-row class="py-7">
         <slot/>
+        <v-expand-transition>
+          <slot
+            v-if="expand"
+            name="expand"/>
+        </v-expand-transition>
+      </v-row>
+
+      <v-row
+          class="bottom justify-center"
+          style="z-index: 20">
+        <v-btn
+            v-if="hasTarget"
+            class="gradient bounce"
+            icon="$expand"
+            @click="goto(props.target)"
+          />
+        <v-btn
+          v-if="hasExpand"
+          variant="text"
+          :icon="expand ? '$minus' : '$plus'"
+          @click="expand = !expand"
+        />
+      </v-row>
+
+      <v-row
+          class="bottom left mt-auto"
+          style="z-index: 10">
+        <slot name="bottom"/>
       </v-row>
     </v-container>
   </v-parallax>
 </template>
 
 <script setup>
+import { useGoTo } from 'vuetify'
+
 const props = defineProps({
   center: {
     type: Boolean,
@@ -19,15 +56,55 @@ const props = defineProps({
   src: {
     type: String,
     required: true
+  },
+  target: {
+    type: String,
+    default: undefined
+  },
+  fill: {
+    type: Boolean,
+    default: false
+  },
+  fullscreen: {
+    type: Boolean,
+    default: false
+  },
+  shadow: {
+    type: Boolean,
+    default: false
   }
 })
+
+const goto = useGoTo()
+
+const slots = useSlots()
+
+const expand = defineModel({ default: false })
+
+const hasTarget = computed(() => props.target !== undefined)
+const hasExpand = computed(() => !!slots.expand)
+const hasBottom = computed(() => hasTarget || hasExpand)
 </script>
 
 <style scoped lang="sass">
+.banner:has(+.clip-right) .bottom
+  bottom: 56px !important
+
 .container
   color: white
-  background: #212121aa
+  // background: rgba(33, 33, 33, 0.56)
   margin: 0
   height: 100%
   max-width: 100vw !important
+
+.bottom
+  position: absolute
+  bottom: 12px
+
+.left
+  position: absolute
+  left: 12px
+
+.fullscreen
+  height: 105vh
 </style>
