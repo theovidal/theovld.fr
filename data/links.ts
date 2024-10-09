dc84febf594f481eabc56187adc45ff0992f0804
Théo Vidal <theo.vidal@ensta-paris.fr>
2024-10-09 14:23:53 +0200

    Update links.ts

--- data/links.ts
+++ data/links.ts
@@ -1,4 +1,5 @@
 export default {
   'notion': 'https://theo-vidal.notion.site/La-biblio-de-Th-o-f8fbe148bf2d4f9fa795ec553ec49eb0',
-  'apprentissage': 'https://theo-vidal.notion.site/Apprentissage-699563a362bc445fa09f7331b3644397'
+  'apprentissage': 'https://theo-vidal.notion.site/Apprentissage-699563a362bc445fa09f7331b3644397',
+  'ea206': 'https://enstafr-my.sharepoint.com/:f:/g/personal/theo_vidal_ensta-paris_fr/EjmVjgpB-WZOvj_kL1cetPUBa_Mgoz5Fd7OTxFJ_ArUOcQ?e=jTAiRd'
 }
